8b072df8c1bf3c31d4d03752672fed15adb2234f
ajay1706 <ajay97456@gmail.com>
2019-09-26 22:19:44 +0530

    fixing bugs

--- cryptocurrency/lib/home_page.dart
+++ cryptocurrency/lib/home_page.dart
@@ -1,35 +1,18 @@
-import 'dart:convert';
-import 'package:async/async.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 
 class HomePage extends StatefulWidget {
+  final List currencies;
+  HomePage(this.currencies);
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
 
+   List currencies;
+  final List<MaterialColor> _colors = [Colors.indigo, Colors.red];
 
-  List currencies ;
-final List<MaterialColor> _colors = [Colors.indigo,Colors.red];
-
-
-@override
-  void initState()  {
-super.initState();
-getCurrencies();    
-    
-  }
-
-Future<List> getCurrencies() async{
-  String cryptoUrl = "https://api.coinmarketcap.com/v1/ticker/";
-  http.Response response = await http.get(cryptoUrl);
-
-  return jsonDecode(response.body);
-
-
-}
 
 
   @override
@@ -44,74 +27,60 @@ Future<List> getCurrencies() async{
 
   Widget _cryptoWidget() {
     return Container(
-      child: Flexible(
-        child: ListView.builder(
-          itemCount: currencies.length,
-          itemBuilder: (BuildContext context, int index){
-            final Map currency = currencies[index];
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              itemCount: widget.currencies.length,
+              itemBuilder: (BuildContext context, int index) {
+                final Map currency = widget.currencies[index];
 
-            final MaterialColor color = _colors[index % _colors.length];
-            return _getListItemUI(currency, color);
-
-
-
-
-          },
-        ),
+                final MaterialColor color = _colors[index % _colors.length];
+                return _getListItemUI(currency, color);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 
+  Widget _getListItemUI(Map currency, MaterialColor color) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: color,
+        child: Text(currency['name'][0]),
+      ),
+      title: Text(
+        currency['name'],
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: _getSubtitleText(
+          currency['price_usd'], currency['percent_change_1h']),
+      trailing: CircleAvatar(
+        child: Text(currency['rank']),
+      ),
+      isThreeLine: true,
+    );
+  }
 
-Widget _getListItemUI(Map currency, MaterialColor color){
-  return ListTile( 
-    leading: CircleAvatar(backgroundColor: color,
-    child: Text(currency['name'][0]), ),
-    title:Text(currency['name'],
-    style: TextStyle(fontWeight: FontWeight.bold)
-    ,),
-    subtitle:_getSubtitleText(currency['price_usd'], currency['percent_change_1h'] ) ,
-    trailing: CircleAvatar( 
-      child: Text(currency['rank']),
-    ),
-    isThreeLine: true,
-     
-  );
-}
+  Widget _getSubtitleText(String priceUSD, String percentageChange) {
+    TextSpan priceTextWidget =
+        TextSpan(text: "\$$priceUSD\n", style: TextStyle(color: Colors.black));
 
+    String percentageChangeText = "1 hour: $percentageChange%";
+    TextSpan percentageChangeTextWidget;
 
+    if (double.parse(percentageChange) > 0) {
+      percentageChangeTextWidget = TextSpan(
+          text: percentageChangeText, style: TextStyle(color: Colors.green));
+    } else {
+      percentageChangeTextWidget = TextSpan(
+          text: percentageChangeText, style: TextStyle(color: Colors.red));
+    }
 
-Widget _getSubtitleText(String priceUSD, String percentageChange){
-
-TextSpan priceTextWidget = TextSpan(
-  text: "\$$priceUSD\n",
-  style: TextStyle(color: Colors.black)
-);
-
-String percentageChangeText = "1 hour: $percentageChange%";
-TextSpan percentageChangeTextWidget;
-
-if(double.parse(percentageChange)>0){
-  percentageChangeTextWidget = TextSpan(  
-    text: percentageChangeText,
-    style: TextStyle(color: Colors.green)
-  );
-
-}
-else {
-  percentageChangeTextWidget = TextSpan(  
-    text: percentageChangeText,
-    style: TextStyle(color: Colors.red)
-  );
-  
-}
-
-return RichText(
-text: TextSpan(
-  children: [priceTextWidget,percentageChangeTextWidget]
-),
-);
-
-}
-
+    return RichText(
+      text: TextSpan(children: [priceTextWidget, percentageChangeTextWidget]),
+    );
+  }
 }
--- cryptocurrency/lib/main.dart
+++ cryptocurrency/lib/main.dart
@@ -1,12 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import './home_page.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+
+void main() async{
+  
+  List currencies =   await  getCurrencies();
+  print(currencies);
+
+   runApp(MyApp(currencies));
+
+
+}
 class MyApp extends StatelessWidget {
+  final List _currencies;
+  MyApp(this._currencies);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: HomePage(_currencies),
       theme: ThemeData(
         primarySwatch: Colors.green
       ),
@@ -14,3 +28,11 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+  Future<List> getCurrencies() async {
+    String cryptoUrl = "https://api.coinmarketcap.com/v1/ticker/";
+    http.Response response = await http.get(cryptoUrl);
+
+    return jsonDecode(response.body);
+  }
